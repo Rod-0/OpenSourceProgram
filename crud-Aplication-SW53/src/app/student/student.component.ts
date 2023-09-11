2995e717a529293afa5b13371e58562476c40ac0
Rodrigo Pozo <rodrigo.pozo25@gmail.com>
2023-09-11 15:19:17 -0500

    add sort and filter

--- crud-Aplication-SW53/src/app/student/student.component.ts
+++ crud-Aplication-SW53/src/app/student/student.component.ts
@@ -1,10 +1,11 @@
 import {Student} from '../models/student.model';
-import { Component,OnInit,ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import {Component, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import {NgForm} from '@angular/forms';
 import {MatPaginator} from '@angular/material/paginator';
-import { HttpDataService } from '../services/http-data.service';
+import {HttpDataService} from '../services/http-data.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-student',
@@ -13,92 +14,101 @@ import { HttpDataService } from '../services/http-data.service';
 })
 export class StudentComponent {
 
-studentData!: Student;
-@ViewChild('studentForm',{static:false})
-studentForm!: NgForm;
+  studentData!: Student;
+  @ViewChild('studentForm', {static: false})
+  studentForm!: NgForm;
 
-dataSource = new MatTableDataSource();
-displayedColumns: string[] = ['id', 'name', 'age', 'mobile', 'email','address','age','action'];
+  dataSource   = new MatTableDataSource();
 
-@ViewChild(MatPaginator,{static:true})
-paginator!: MatPaginator;
-isEditMode = false;
+  displayedColumns: string[] = ['id', 'name', 'age', 'mobile', 'email', 'address', 'age', 'action'];
 
-@ViewChild(MatSort,{static:true})
-sort!: MatSort;
+  @ViewChild(MatPaginator, {static: true})
+  paginator!: MatPaginator;
+  isEditMode = false;
 
-onSubmit() {
-  if(this.studentForm.form.valid){
-    console.log('valid');
-    if(this.isEditMode){
-      this.updateStudent()
-    }else{
-      console.log("create")
-      this.addStudnet();
+  @ViewChild(MatSort, {static: true})
+  sort!: MatSort;
+
+  onSubmit() {
+    if (this.studentForm.form.valid) {
+      console.log('valid');
+      if (this.isEditMode) {
+        console.log("update")
+        this.updateStudent()
+      } else {
+        console.log("create")
+        this.addStudnet();
+      }
+      this.cancelEdit();
+
+    } else {
+      console.log('invalid');
     }
-    this.cancelEdit();
-
-  }else{
-    console.log('invalid');
   }
-}
-constructor(private httpDataService:HttpDataService){
-  this.studentData = {} as Student;
-}
+
+  constructor(private httpDataService: HttpDataService) {
+    this.studentData = {} as Student;
+  }
 
 
+  ngOnInit(): void {
+    this.getAllStudent();
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
 
-ngOnInit():void{  
-  this.getAllStudent();
-  this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
-}
+  }
 
-getAllStudent(){
-  this.httpDataService.getList().subscribe((response:any)=>{
-    this.dataSource.data = response;
-  })
-}
+  getAllStudent() {
+    this.httpDataService.getList().subscribe((response: any) => {
+      this.dataSource.data = response;
+    })
+  }
 
-editItem(element:any){
-  this.studentData = element; //VALIDATE
-  this.isEditMode = true;
-}
+  editItem(element: any) {
 
-cancelEdit(){
-  this.isEditMode = false;
-  this.studentForm.resetForm();
-}
+    this.studentData = _.cloneDeep(element); //VALIDATE
+    this.isEditMode = true;
+  }
+
+  cancelEdit() {
+    this.isEditMode = false;
+    this.studentForm.resetForm();
+  }
 
 //delete
-deleteItem(id:string){
-  this.httpDataService.deleteItem(id).subscribe((response:any)=>{
-    this.dataSource.data = this.dataSource.data.filter((o:any)=>{
-      return o.id !== id ? o : false;
-    });
-  })
-
-  console.log(this.dataSource.data)
-}
-//add
-addStudnet(){
-  this.httpDataService.createItrem(this.studentData).subscribe((response:any)=>{
-    this.dataSource.data.push({...response});
-    this.dataSource.data = this.dataSource.data.map((o:any)=>{
-      return o;
+  deleteItem(id: string) {
+    this.httpDataService.deleteItem(id).subscribe((response: any) => {
+      this.dataSource.data = this.dataSource.data.filter((o: any) => {
+        return o.id !== id ? o : false;
+      });
     })
-  }) 
-}
+
+    console.log(this.dataSource.data)
+  }
+
+//add
+  addStudnet() {
+    this.httpDataService.createItem(this.studentData).subscribe((response: any) => {
+      this.dataSource.data.push({...response});
+      this.dataSource.data = this.dataSource.data.map((o: any) => {
+        return o;
+      })
+    })
+  }
 
 //update
-updateStudent(){
-  this.httpDataService.updateItem(this.studentData.id,this.studentData).subscribe((response:any)=>{
-    this.dataSource.data = this.dataSource.data.map((o:any)=>{
-      if(o.id === response.id){
-        o = response;
-      }
-      return o;
+  updateStudent() {
+    this.httpDataService.updateItem(this.studentData.id, this.studentData).subscribe((response: any) => {
+      this.dataSource.data = this.dataSource.data.map((o: any) => {
+        if (o.id === response.id) {
+          o = response;
+        }
+        return o;
+      })
     })
-  })
-}
+  }
+
+  Filterchange(event: Event) {
+    this.dataSource.filter = (event.target as HTMLInputElement).value;
+  }
 }
